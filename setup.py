44a4288f165105f417e8c03ae0aafc79dbf39d75
blackshoals <blackshoals@users.noreply.github.com>
2023-05-23 17:29:20 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="chs-tides",
-    version="0.3.1",
-    author="Ron Schofield",
-    author_email="ronschofield@eastlink.ca",
+    name="canadatides",
+    version="0.1",
+    author="blackshoals",
+    author_email="",
     description="Python Wrapper for Canadian Hydrographic Service (CHS) Water Level System API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/RonSchofield/pychs",
+    url="",
     packages=setuptools.find_packages(),
     install_requires=[
         "aiohttp",
